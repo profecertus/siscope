95314010b47b95ff2c4adb6b55319e41e0b9b7e3
edwinBarrientosRetuerto <edwin.barrientos.retuerto@gmail.com>
2023-11-05 05:14:54 -0500

    Adicion del Save Trabajador

--- src/main/java/pe/com/isesystem/gpservice/controller/TrabajadorController.java
+++ src/main/java/pe/com/isesystem/gpservice/controller/TrabajadorController.java
@@ -6,10 +6,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pe.com.isesystem.gpservice.configuration.RespuestaHttp;
 import pe.com.isesystem.gpservice.dto.CamaraDto;
 import pe.com.isesystem.gpservice.dto.TrabajadorDto;
 import pe.com.isesystem.gpservice.service.TrabajadorService;
@@ -26,5 +24,13 @@ public class TrabajadorController {
         final Pageable pageable = PageRequest.of(numpag, tampag );
         Page<TrabajadorDto> respuesta = trabajadorService.getAllTrabajador(pageable);
         return new ResponseEntity<>(respuesta, HttpStatus.OK);
+    }
+
+    @PostMapping("/saveTrabajador")
+    public ResponseEntity<RespuestaHttp> saveTrabajador(@RequestBody TrabajadorDto trabajadorDto){
+        TrabajadorDto trab = trabajadorService.saveTrabajador(trabajadorDto);
+        RespuestaHttp respuestaHttp = new RespuestaHttp();
+        respuestaHttp.setValorDevuelto(trab);
+        return new ResponseEntity<>(respuestaHttp, HttpStatus.OK);
     }
 }
