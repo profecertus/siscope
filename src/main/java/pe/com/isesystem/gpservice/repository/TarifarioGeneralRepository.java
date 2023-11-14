49fa455ece652cc2e96b30611cb415d5230bee87
edwinBarrientosRetuerto <edwin.barrientos.retuerto@gmail.com>
2023-11-14 01:57:46 -0500

    Adecuación de Moneda

--- src/main/java/pe/com/isesystem/gpservice/repository/TarifarioGeneralRepository.java
+++ src/main/java/pe/com/isesystem/gpservice/repository/TarifarioGeneralRepository.java
@@ -1,6 +1,7 @@
 package pe.com.isesystem.gpservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,9 +11,10 @@ import pe.com.isesystem.gpservice.model.TarifarioGeneralId;
 @Repository
 public interface TarifarioGeneralRepository extends JpaRepository<TarifarioGeneral, TarifarioGeneralId> {
 
+    @Modifying
     @Query(value = "INSERT INTO TARIFARIO_GENERAL(id_proveedor, id_tipo_servicio, id_dia, monto, estado, estado_reg) VALUES(:idProveedor, :idTipoServicio, :idDia, 0,true, true)",
            nativeQuery = true)
-    <S extends TarifarioGeneral> S  insertTarifario(@Param("idProveedor") Long idProveedor, @Param("idTipoServicio") Long idTipoServicio, @Param("idDia") Long idDia);
+    void  insertTarifario(@Param("idProveedor") Long idProveedor, @Param("idTipoServicio") Long idTipoServicio, @Param("idDia") Long idDia);
 
 }
 
