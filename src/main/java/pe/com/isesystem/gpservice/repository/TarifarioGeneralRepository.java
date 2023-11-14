d79a33661775177ff5fb85a1676c198ad3985970
edwinBarrientosRetuerto <edwin.barrientos.retuerto@gmail.com>
2023-11-14 02:11:53 -0500

    Adecuación de Moneda

--- src/main/java/pe/com/isesystem/gpservice/repository/TarifarioGeneralRepository.java
+++ src/main/java/pe/com/isesystem/gpservice/repository/TarifarioGeneralRepository.java
@@ -12,7 +12,8 @@ import pe.com.isesystem.gpservice.model.TarifarioGeneralId;
 public interface TarifarioGeneralRepository extends JpaRepository<TarifarioGeneral, TarifarioGeneralId> {
 
     @Modifying
-    @Query(value = "INSERT INTO TARIFARIO_GENERAL(id_proveedor, id_tipo_servicio, id_dia, monto, estado, estado_reg) VALUES(:idProveedor, :idTipoServicio, :idDia, 0,true, true)",
+    @Query(value = "INSERT INTO TARIFARIO_GENERAL(id_proveedor, id_tipo_servicio, id_dia, monto, estado, estado_reg) " +
+            "SELECT :idProveedor, :idTipoServicio, CAST(TO_CHAR(current_date AT TIME ZONE 'America/Lima', 'YYYYMMDD') AS BIGINT), 0, true, true FROM DUAL",
            nativeQuery = true)
     void  insertTarifario(@Param("idProveedor") Long idProveedor, @Param("idTipoServicio") Long idTipoServicio, @Param("idDia") Long idDia);
 
