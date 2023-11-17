8c7a28127ce81d4741618f7b8f81f2c5335f2d72
edwinBarrientosRetuerto <edwin.barrientos.retuerto@gmail.com>
2023-11-17 16:51:40 -0500

    Adecuación del Ubigeo

---
+++ src/main/java/pe/com/isesystem/gpservice/repository/TarifarioPlantaRepository.java
@@ -0,0 +1,36 @@
+package pe.com.isesystem.gpservice.repository;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pe.com.isesystem.gpservice.model.TarifarioPlanta;
+import pe.com.isesystem.gpservice.model.TarifarioPlantaId;
+
+@Repository
+public interface TarifarioPlantaRepository extends JpaRepository<TarifarioPlanta, TarifarioPlantaId> {
+
+    @Modifying
+    @Query(value = "INSERT INTO TARIFARIO_PLANTA (id_dia, id_planta, id_proveedor, id_tipo_servicio, " +
+            "id_moneda, monto, estado, estado_reg) " +
+            "SELECT CAST(TO_CHAR(current_date AT TIME ZONE 'America/Lima', 'YYYYMMDD') AS NUMERIC), :idPlanta, " +
+            ":idProveedor, :idTipoServicio, 1, 0, true, true FROM DUAL",nativeQuery = true)
+    void  insertTarifarioPlanta(@Param("idPlanta") Long idPlanta,
+                                @Param("idProveedor") Long idProveedor,
+                                @Param("idTipoServicio") Long idTipoServicio);
+
+    @Query(value = "SELECT COALESCE(SUM(id_proveedor), 0) FROM TARIFARIO_PLANTA " +
+            "WHERE id_planta = :idPlanta  and " +
+            "id_tipo_servicio = :idTipoServicio",nativeQuery = true)
+    Long existe(@Param("idPlanta") Long idPlanta,
+                @Param("idTipoServicio") Long idTipoServicio);
+
+    @Modifying
+    @Query(value = "DELETE FROM TARIFARIO_PLANTA " +
+            " WHERE id_planta = :idPlanta AND " +
+            " id_proveedor = :idProveedor AND " +
+            " id_tipo_servicio = :idServicio ", nativeQuery = true)
+    void eliminarTarifarioPlanta(@Param("idPlanta") Long idPlanta,
+                                 @Param("idProveedor") Long idProveedor,
+                                 @Param("idTipoServicio") Long idTipoServicio);
+
+}
--- src/main/java/pe/com/isesystem/gpservice/service/RelPlantaProveedorService.java
+++ src/main/java/pe/com/isesystem/gpservice/service/RelPlantaProveedorService.java
@@ -3,20 +3,30 @@ package pe.com.isesystem.gpservice.service;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import pe.com.isesystem.gpservice.model.TarifarioPlanta;
 import pe.com.isesystem.gpservice.repository.RelPlantaProveedorRepository;
+import pe.com.isesystem.gpservice.repository.TarifarioPlantaRepository;
 
 @Service
 public class RelPlantaProveedorService {
     RelPlantaProveedorRepository relPlantaProveedorRepository;
+    TarifarioPlantaRepository tarifarioPlantaRepository;
     ModelMapper modelMapper;
 
-    public RelPlantaProveedorService(RelPlantaProveedorRepository relPlantaProveedorRepository, ModelMapper modelMapper){
+    public RelPlantaProveedorService(RelPlantaProveedorRepository relPlantaProveedorRepository, ModelMapper modelMapper,
+                                     TarifarioPlantaRepository tarifarioPlantaRepository){
         this.relPlantaProveedorRepository = relPlantaProveedorRepository;
         this.modelMapper = modelMapper;
+        this.tarifarioPlantaRepository = tarifarioPlantaRepository;
     }
 
     @Transactional
     public void actualizarPlantaProveedor(Long idPlanta,  Long idProveedor, Long idTipoServicio){
+        Long idMiProveedor = this.tarifarioPlantaRepository.existe(idPlanta, idTipoServicio);
+        if (  idMiProveedor == 0 || idMiProveedor != idProveedor )
+            this.tarifarioPlantaRepository.insertTarifarioPlanta(idPlanta, idProveedor, idTipoServicio);
+        if (idProveedor != idMiProveedor)
+            this.tarifarioPlantaRepository.eliminarTarifarioPlanta(idPlanta, idMiProveedor, idTipoServicio);
         this.relPlantaProveedorRepository.eliminarRelPlantaProveedor(idPlanta, idTipoServicio);
         this.relPlantaProveedorRepository.grabarRelPlantaProveedor(idPlanta, idProveedor, idTipoServicio);
     }
